e2a3112764559f88cf599f7820b1decc56e48cd8
Poafs1 <poafs1@hotmail.com>
2021-02-20 11:11:21 +0700

    Add connected room input section

---
+++ client/components/chat/v1/connectRoom.tsx
@@ -0,0 +1,60 @@
+import styles from './css/ConnectRoom.module.css'
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+const ConnectRoom = () => {
+  const [username, setUsername] = useState('')
+  const [chatServer, setChatServer] = useState('')
+  
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    switch(name) {
+      case "setUsername":
+        setUsername(value)
+        return
+      case "setChatServer":
+        setChatServer(value)
+      default:
+        return
+    }
+  }
+
+  const handleOnClick = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault()
+  }
+
+  return(
+    <form className={styles.container}>
+      {/* User name */}
+      <div className={styles.form}>
+        <label>User Name:</label>
+        <input
+          type="text"
+          name="setUsername"
+          onChange={handleOnChange}
+          value={username}
+        />
+      </div>
+
+      {/* Chat server */}
+      <div className={styles.form}>
+        <label>Chat Server:</label>
+        <input
+          type="text"
+          name="setChatServer"
+          onChange={handleOnChange}
+          value={chatServer}
+        />
+      </div>
+
+      {/* Connect Button */}
+      <div className={styles.button}>
+        <button type="submit" onClick={handleOnClick}>Connect</button>
+      </div>
+    </form>
+  )
+}
+
+export default ConnectRoom
+
+ConnectRoom.propTypes = {}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,5 @@
 import Layout from '../layouts'
+import ConnectRoom from '../components/chat/v1/connectRoom'
 import SendMessage from '../components/chat/v1/sendMessage'
 import ReceiveMessage from '../components/chat/v1/receiveMessage'
 
@@ -12,6 +13,9 @@ const Index = () => {
   return(
     <Layout {...headerTitle}>
       {/* Connect Section */}
+      <section>
+        <ConnectRoom />
+      </section>
       
       {/* Chat Section */}
       <section>
